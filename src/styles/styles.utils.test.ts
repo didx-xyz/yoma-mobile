5ceaa74273ae8408a38159b064780f1c4fdcd760
Cameron Olivier <cameronolivier@gmail.com>
2021-03-15 23:53:04 +0200

    YOMA-152 feat(Navigation initial setup (#2)
    * chore: updating podlock
    
    * chore: getting tests working, adding prettier and prettier config, adjusting app.json > config.ts, adding help.json to get a list of npm commands for the project
    
    * chore: adding prettier installing plugin, updating files
    
    * feat(ViewContainer): adding View container, and initial colors, along with some utils
    
    * chore: adding pull request template
    
    * chore: adding pull request template
    
    * feat: adding initial pages
    
    * feat(Navigation): adding initial views with navigation and initial/makeshift buttons to navigate between the views
    
    * feat(Navigation): new screens, removing AppNavigation styles as its unused

--- src/styles/styles.utils.test.ts
+++ src/styles/styles.utils.test.ts
@@ -2,13 +2,13 @@ import { applyAlphaToHex } from './styles.utils'
 
 describe('styles/styles.utils', () => {
   describe('applyAlphaToHex', () => {
-    it.each([['#ffffff', 0.5, '#ffffff80']])(
-      'should add an alpha value to the given color hex value',
-      (color, alpha, expected) => {
-        const result = applyAlphaToHex(color)(alpha)
+    it.each([
+      ['#ffffff', 0.5, '#ffffff80'],
+      ['#000000', 0.5, '#00000080'],
+    ])('should add an alpha value to the given color hex value', (color, alpha, expected) => {
+      const result = applyAlphaToHex(color)(alpha)
 
-        expect(result).toBe(expected)
-      },
-    )
+      expect(result).toBe(expected)
+    })
   })
 })
