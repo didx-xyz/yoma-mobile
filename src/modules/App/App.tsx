b5ee5ca314787bc2aec82620e8a33c74f51b2afd
sai.ranjit <sairanjit.tummalapalli@ayanworks.com>
2021-03-31 11:23:28 +0530

    Common error handling

--- src/modules/App/App.tsx
+++ src/modules/App/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 import AppNavigation from '../AppNavigation'
+import FlashMessage from "react-native-flash-message";
 
 const App = () => {
-  return <AppNavigation />
+  return (
+    <>
+      <AppNavigation />
+      <FlashMessage position="top" />
+    </>
+  );
 }
 
 export default App
---
+++ src/utils/error.ts
@@ -0,0 +1,19 @@
+import { showMessage, MessageOptions, MessageType, } from "react-native-flash-message";
+
+export const showSimpleMessage = (
+  type: MessageType = "default",
+  message: string = "Message",
+  description: string = "",
+  props = {}
+) => {
+  const messageOptions: MessageOptions = {
+    message: message,
+    description: description,
+    icon: { icon: "auto", position: "left" },
+    type,
+    duration: 2500,
+    ...props,
+  };
+
+  showMessage(messageOptions);
+}
