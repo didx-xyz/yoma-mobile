2ffdbea2241d03b8bb3ff53849ae884757c64b4d
sai.ranjit <sairanjit.tummalapalli@ayanworks.com>
2021-03-31 11:20:12 +0530

    Added dropdown component

---
+++ src/components/DropDown/DropDown.styles.ts
@@ -0,0 +1,23 @@
+import { Dimensions, StyleSheet, ViewStyle } from 'react-native'
+
+const { height, width } = Dimensions.get("window")
+
+const styles = {
+  dropDownStyle: {
+    backgroundColor: 'rgb(243,246,250)',
+    width: width / 1.3,
+    alignSelf: 'center',
+    borderWidth: 0,
+    borderTopLeftRadius: 60,
+    borderTopRightRadius: 60,
+    borderBottomLeftRadius: 60,
+    borderBottomRightRadius: 60,
+  } as ViewStyle,
+  dropDownViewStyle: {
+    backgroundColor: 'rgb(243,246,250)',
+    width: "90%",
+    alignSelf: 'center',
+  } as ViewStyle,
+}
+
+export default StyleSheet.create(styles)
