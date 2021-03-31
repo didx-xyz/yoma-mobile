a3a534a181d9f36a9c09da5305e312afb317f4e7
sai.ranjit <sairanjit.tummalapalli@ayanworks.com>
2021-03-31 11:06:10 +0530

    Api setup and added facebook and google package for signin

--- src/styles/button.styles.ts
+++ src/styles/button.styles.ts
@@ -6,7 +6,7 @@ const { height, width } = Dimensions.get("window")
 
 
 const ButtonStyles = StyleSheet.create({
-  mediumGreenButton: {
+  mediumtertiary3Button: {
     backgroundColor: colors[Colors.tertiary3],
     height: height / 13,
     alignItems: 'center',
@@ -19,6 +19,34 @@ const ButtonStyles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   } as ViewStyle,
+  largeTertiary3Button: {
+    backgroundColor: colors[Colors.tertiary3],
+    height: height / 13,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: width / 1.3,
+    borderRadius: 60,
+  } as ViewStyle,
+  facebookButton: {
+    backgroundColor: '#507CC0',
+    height: height / 13,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    width: width / 1.3,
+    borderRadius: 60,
+    flexDirection: 'row'
+  } as ViewStyle,
+  googleButton: {
+    backgroundColor: colors[Colors.white],
+    height: height / 13,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    width: width / 1.3,
+    borderRadius: 60,
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: '#DF4930'
+  } as ViewStyle
 })
 
 export default ButtonStyles;
