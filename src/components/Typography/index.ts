e6a17dee1701c4eeb026f82b16378f995757a372
Cameron Olivier <cameronolivier@gmail.com>
2021-04-11 23:21:45 +0200

    refactor(Typography): creating Text constant for export

--- src/components/Typography/index.ts
+++ src/components/Typography/index.ts
@@ -7,10 +7,12 @@ import Meta from './Meta/Meta'
 import { MetaLevels } from './Meta/Meta.types'
 import { FontWeights, TextAlign } from './Text.types'
 
-export default {
+const Text = {
   Header,
   Body,
   Meta,
 }
+
+export default Text
 
 export { Bold, HeaderLevels, BodyLevels, MetaLevels, FontWeights, TextAlign }
