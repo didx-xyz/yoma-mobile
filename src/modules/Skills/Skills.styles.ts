8fd4754823472f05e89c90008d3db1ba9bea9f6d
sai.ranjit <sairanjit.tummalapalli@ayanworks.com>
2021-04-19 23:37:21 +0530

    feat(skills list): adding skills list UI

--- src/modules/Skills/Skills.styles.ts
+++ src/modules/Skills/Skills.styles.ts
@@ -1,10 +1,42 @@
 import { StyleSheet, ViewStyle } from 'react-native'
 import { Colors, colors } from 'styles'
+import { applyAlphaToHex } from 'styles/styles.utils'
 
 const styles = {
   container: {
     backgroundColor: colors[Colors.backgroundGrey],
     ...StyleSheet.absoluteFillObject,
+  } as ViewStyle,
+  whiteCard: {
+    backgroundColor: colors[Colors.white],
+    width: '95%',
+    borderRadius: 12,
+    paddingVertical: 20,
+    elevation: 3,
+    marginVertical: 10,
+    alignSelf: 'center',
+    paddingHorizontal: 10,
+  } as ViewStyle,
+  certificateCountView: {
+    height: 30,
+    width: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 5,
+    borderRadius: 15,
+    marginRight: 10,
+    backgroundColor: applyAlphaToHex(colors[Colors.primaryBlue])(0.15),
+  } as ViewStyle,
+  cardHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingBottom: 10,
+  } as ViewStyle,
+  bodyItemView: {
+    paddingVertical: 15,
+    padding: 5,
+    borderTopWidth: 1,
+    borderColor: colors[Colors.grey4],
   } as ViewStyle,
 }
 
--- src/modules/Skills/Skills.tsx
+++ src/modules/Skills/Skills.tsx
@@ -1,8 +1,9 @@
 import { NormalHeader, ViewContainer } from 'components'
-import Text from 'components/Typography'
+import Text, { BodyLevels, HeaderLevels } from 'components/Typography'
 import React, { useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { FlatList } from 'react-native'
+import { FlatList, View } from 'react-native'
+import { Colors } from 'styles'
 
 import styles from './Skills.styles'
 import SkillsForm from './SkillsForm/Skills'
@@ -15,7 +16,28 @@ const Skills = ({ navigation }: Props) => {
   const { t } = useTranslation()
   const [isSave, setIsSave] = useState(false)
   // TODO: adding static data for UI
-  const [skills, setSkills] = useState([])
+  const [skills, setSkills] = useState([
+    {
+      skill: 'Graphic Design',
+      count: 3,
+    },
+    {
+      skill: 'Photograph',
+      count: 45,
+    },
+    {
+      skill: 'Illustration',
+      count: 12,
+    },
+    {
+      skill: 'Figma',
+      count: 3,
+    },
+    {
+      skill: 'Keynote',
+      count: 22,
+    },
+  ])
   const formRef = useRef<any>()
 
   return (
@@ -36,7 +58,30 @@ const Skills = ({ navigation }: Props) => {
           <SkillsForm />
         </>
       ) : (
-        <FlatList data={skills} renderItem={() => <Text.Body>render</Text.Body>} />
+        <View style={styles.whiteCard}>
+          <View style={styles.cardHeader}>
+            <View style={styles.certificateCountView}>
+              <Text.Header level={HeaderLevels.h6} color={Colors.primaryBlue}>
+                60
+              </Text.Header>
+            </View>
+            <Text.Header level={HeaderLevels.h5} color={Colors.primaryPurple}>
+              {t('Top skills')}
+            </Text.Header>
+          </View>
+          <FlatList
+            data={skills}
+            renderItem={({ item }) => (
+              <View style={styles.bodyItemView}>
+                <Text.Header level={HeaderLevels.h6}>
+                  {item.skill}
+                  <Text.Body level={BodyLevels.small}> • {item.count}</Text.Body>
+                </Text.Header>
+              </View>
+            )}
+            keyExtractor={item => item.skill}
+          />
+        </View>
       )}
     </ViewContainer>
   )
