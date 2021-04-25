feab61b880abd45862939340f9af8f13821de76b
Cameron Olivier <cameronolivier@gmail.com>
2021-04-25 15:20:08 +0200

    refactor(Login): typing dispatch

--- src/modules/Login/Login.container.ts
+++ src/modules/Login/Login.container.ts
@@ -1,8 +1,9 @@
 import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
 
 import Login from './Login'
 
 const mapStateToProps = null
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = (dispatch: Dispatch) => ({})
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login)
