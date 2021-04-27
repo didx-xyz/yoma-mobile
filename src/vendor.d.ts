2ec31206892738f1bdd6e6103ecfb62c12f1923d
Cameron Olivier <cameronolivier@gmail.com>
2021-04-27 19:14:02 +0200

    feat(Redux, Auth): wiring login to use redux, some additional changes as required to test

--- src/vendor.d.ts
+++ src/vendor.d.ts
@@ -1,13 +1,22 @@
 declare module 'ramda' {
+  export function __(...rest: any[]): any[]
+  export function always(...rest: any[]): any[]
   export function append(...rest: any[]): any
   export function concat(...rest: any[]): any[]
   export function curry(...rest: any[]): any[]
+  export function isNil(...rest: any[]): any[]
   export function join(...rest: any[]): string
   export function mergeAll(...rest: any[]): any
+  export function mergeDeepRight(...rest: any[]): any
   export function multiply(...rest: any[]): number
+  export function objOf(...rest: any[]): any[]
   export function of(...rest: any[]): any[]
   export function pipe(...rest: any[]): any
+  export function path(...rest: any[]): any
+  export function pick(...rest: any[]): any
   export function prepend(...rest: any[]): any
+  export function reduceRight(...rest: any[]): any[]
   export function tap(...rest: any[]): any[]
-  export function mergeDeepRight(...rest: any[]): any[]
+  export function unless(...rest: any[]): any[]
+  export function when(...rest: any[]): any[]
 }
