f99cd020a36c724ac6d08b394243834c9e10b17e
Cameron Olivier <cameronolivier@gmail.com>
2021-04-27 21:58:37 +0200

    YOMA-218  - feat(Redux): setup redux boilerplate on Login (#23)
    * chore: adding mergeDeepRight typescript
    
    * chore: updating to get ios working
    
    * chore: adding alias for assets to babel config
    
    * feat(Redux, Auth): wiring login to use redux, some additional changes as required to test
    
    * chore: removing comments

---
+++ src/modules/Login/Login.tsx
@@ -0,0 +1,68 @@
+import { YellowCircleLeft } from 'assets/images'
+import { LargeHeaderContainer, SocialRegistration, ViewContainer } from 'components'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { ScrollView, TouchableOpacity, View } from 'react-native'
+import { colors, Colors } from 'styles'
+
+import Text, { HeaderLevels, Span, TextAlign } from '../../components/Typography'
+import { AuthCredentials } from '../Auth/Auth.types'
+import styles from './Login.styles'
+import LoginForm from './LoginForm/LoginForm'
+
+interface Props {
+  navigation: any
+  onLoginUser: (details: AuthCredentials) => void
+}
+
+const Login = ({ navigation, onLoginUser }: Props) => {
+  const { t } = useTranslation()
+  return (
+    <ViewContainer style={styles.container}>
+      <ScrollView>
+        <LargeHeaderContainer
+          navigation={navigation}
+          headerText={t('login')}
+          backgroundColor={colors[Colors.primaryPurple]}
+          circleImage={<YellowCircleLeft />}
+          circleImageStyle={styles.yellowSemiCircleContainer}
+        />
+        <View style={styles.whiteCard}>
+          <Text.Header level={HeaderLevels.h3} align={TextAlign.center} style={styles.cardHeader}>
+            {t('welcomeBack')}
+          </Text.Header>
+          <LoginForm onLoginUser={onLoginUser} />
+          <TouchableOpacity onPress={() => navigation.navigate('ForgotPassword')}>
+            <Text.Header level={HeaderLevels.h5} style={styles.forgotPassword}>
+              {t('forgotPassword')} ?
+            </Text.Header>
+          </TouchableOpacity>
+          <View style={styles.horizontalLineView}>
+            <View style={styles.horizontalLine} />
+            <Text.Header level={HeaderLevels.h3} color={Colors.primaryDarkGrey} style={styles.orText}>
+              {t('or')}
+            </Text.Header>
+            <View style={styles.horizontalLine} />
+          </View>
+          <Text.Header level={HeaderLevels.h5} style={styles.loginSocial}>
+            {t('loginSocial')}
+          </Text.Header>
+          <SocialRegistration />
+        </View>
+        <Text.Header
+          level={HeaderLevels.h5}
+          color={Colors.primaryDarkGrey}
+          align={TextAlign.center}
+          style={styles.noAccount}
+        >
+          {t('noAccount')}
+          <Span color={Colors.primaryGreen} align={TextAlign.center} onPress={() => navigation.navigate('Register')}>
+            &nbsp; {t('registerHere')}.
+          </Span>
+        </Text.Header>
+      </ScrollView>
+    </ViewContainer>
+  )
+}
+
+export default Login
