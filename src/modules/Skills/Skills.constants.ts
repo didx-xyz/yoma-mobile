428d09615ef12ff15fb02e4101c34b3bcf8933cf
sairanjitAW <75250487+sairanjitAW@users.noreply.github.com>
2021-05-05 00:47:20 +0530

    Yoma 279 skills list page (#16)
    * added bottom tab bar with icons
    
    * added UI cards with styles
    
    * ran prettier
    
    * added profile photo component
    
    * Added custom textinputs
    
    * added rn fetch blob and image picker package
    
    * added props for header component
    
    * Profile page update
    
    * initial about us page setup
    
    * about updation completed
    
    * added test for rnfetchblob and added modal
    
    * format:ran prettier
    
    * chore: adding experience page initial setup and audit-ci package
    
    * feat(Experience form): adding basic form
    
    * chore: adding datepicker and react-native-tags package
    
    * feat(Experience): api call to save and create credential
    
    * feat(datepicker): changing date picker package for compatibility
    
    * feat(organizations Dropdown): select organization from dropdown
    
    * feat(skill) : adding skills and deleting with tags
    
    * feat(form validation): adding validation in experience page
    
    * feat(experience card): adding work experience card and styling
    
    * refactor(typograghy): adding cutom text into components
    
    * refactor(translation): updating texts with multiilanguage json files
    
    * refactor(location): manually add region in work experience
    
    * refactor(constants): adding api constants and generate endpoint function
    
    * feat(Education): adding ui for education page
    
    * refactor(Optional component): adding optional component for using the conditional case
    
    * refactor(Upload): adding upload component into components folder
    
    * feat(edit education): adding edit education ui
    
    * feat(add skills): adding skills component
    
    * feat(skills list): adding skills list UI
    
    * feat(skills acquired): adding button to each skill
    
    * refactor(styling): removing absolute positioning and adding typescript types
    
    * refactor(date and colors): adding difference between dates function and colors with opacity
    
    * refactor(styling): removing width and handling UI with flex and margin
    
    * refactor(types): adding types for organization and dropdowns
    
    * refactor(imports): adding index.ts file in all components
    
    * refactor(colorcard): adding card component with default color
    
    * refactor(color): changing unsaved color and adding it into color styles
    
    * refactor(infocard): adding infocard into components and added function with tests
    
    * refactor(styles): added constant values into separate file
    
    * refactor(skill card): adding skill card into component
    
    * refactor(color): changed color of the upload component view
    
    * refactor(date): refactoring date function and test
    
    * refactor(styles): removing contants from styles
    
    * refactor(Experience utils): added functions to separate file and added test
    
    * refactor(styles): ensured that all styles follow naming convention in all screens
    
    * refactor(placeholder): adding placeholder color from color styles
    
    * refactor(naming): changing naming convention
    
    * refactor(styles): removing constants and ensuring naming conventions
    
    * refactor(useRef): adding typescript to useref
    
    * refactor(styles): removing all constants from the styles and adding types wherever required
    
    * refactor(Eductation): adding type to education data
    
    * refactor(state): chhaging name of state in education
    
    * refactor(skills): separating functions and components and added tests for functions
    
    * refactor(deleteItem): adding the namimg convention
    
    * refactor(skill card): changing the naming convention and inline style constants
    
    * refactor(styles): changing style properties naming convention
    
    * refactor(getFirstCharAndUppercase): changing name to getUppercasedHead
    
    * refactor(styles): changing style properties names
    
    * refactor(styles): changin style property name and added dot in card
    
    * refactor(text): adding missing text colors
    
    * refactor(NormalHeader): adding correct type to the headerText
    
    * refactor(state): changing state naming convention

---
+++ src/modules/Skills/Skills.constants.ts
@@ -0,0 +1,22 @@
+export const MOCK_SKILLS = [
+  {
+    skill: 'Graphic Design',
+    count: 3,
+  },
+  {
+    skill: 'Photograph',
+    count: 45,
+  },
+  {
+    skill: 'Illustration',
+    count: 12,
+  },
+  {
+    skill: 'Figma',
+    count: 3,
+  },
+  {
+    skill: 'Keynote',
+    count: 22,
+  },
+]
--- src/modules/Skills/Skills.tsx
+++ src/modules/Skills/Skills.tsx
@@ -1,9 +1,11 @@
-import { NormalHeader, Optional, ViewContainer } from 'components'
-import Text from 'components/Typography'
+import { Card, NormalHeader, Optional, SkillCard, ViewContainer } from 'components'
+import Text, { HeaderLevels } from 'components/Typography'
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { FlatList } from 'react-native'
+import { FlatList, View } from 'react-native'
+import { Colors } from 'styles'
 
+import { MOCK_SKILLS } from './Skills.constants'
 import styles from './Skills.styles'
 import SkillsForm from './SkillsForm/SkillsForm'
 
@@ -14,20 +16,44 @@ interface Props {
 const Skills = ({ navigation }: Props) => {
   const { t } = useTranslation()
   const [isEditing, setIsEditing] = useState(false)
-  const [skills, setSkills] = useState([])
+  const [skills, setSkills] = useState(MOCK_SKILLS)
 
   return (
     <ViewContainer style={styles.container}>
       <NormalHeader
         navigation={navigation}
-        headerText={t('Skills')}
+        headerText={
+          <Optional condition={isEditing} fallback={t('Skills')}>
+            {t('Add skill')}
+          </Optional>
+        }
         onSave={() => {}}
         onAdd={() => setIsEditing(true)}
         showAddButton={!isEditing}
       />
       <Optional
         condition={isEditing}
-        fallback={<FlatList data={skills} renderItem={() => <Text.Body>render</Text.Body>} />}
+        fallback={
+          <Card style={styles.outerCard}>
+            <FlatList
+              data={skills}
+              ListHeaderComponent={
+                <View style={styles.cardHeader}>
+                  <View style={styles.certificateCount}>
+                    <Text.Header level={HeaderLevels.h6} color={Colors.primaryBlue}>
+                      60
+                    </Text.Header>
+                  </View>
+                  <Text.Header level={HeaderLevels.h5} color={Colors.primaryPurple}>
+                    {t('Top skills')}
+                  </Text.Header>
+                </View>
+              }
+              renderItem={({ item }) => <SkillCard skill={item.skill} skillCount={item.count} onPress={() => {}} />}
+              keyExtractor={item => item.skill}
+            />
+          </Card>
+        }
       >
         <SkillsForm />
       </Optional>
