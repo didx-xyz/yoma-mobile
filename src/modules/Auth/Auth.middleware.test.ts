5cf37bac569cf33304c04058d23c1454e251a983
sai.ranjit <sairanjit.tummalapalli@ayanworks.com>
2021-05-05 23:10:16 +0530

    feat(signup): adding tests to Auth signup middleware

--- src/modules/Auth/Auth.middleware.test.ts
+++ src/modules/Auth/Auth.middleware.test.ts
@@ -1,7 +1,15 @@
 import { createMiddlewareMock } from '../../../tests/tests.utils'
 import * as SUT from './Auth.middleware'
-import { authLogin, authLoginFailure, authLoginSuccess, setAuthCredentials } from './Auth.reducer'
-import { defaultUserLoginResponseData } from './Auth.test.fixtures'
+import {
+  authLogin,
+  authLoginFailure,
+  authLoginSuccess,
+  authRegistration,
+  authRegistrationFailure,
+  authRegistrationSuccess,
+  setAuthCredentials,
+} from './Auth.reducer'
+import { defaultUserLoginResponseData, defaultUserRegistrationResponseData } from './Auth.test.fixtures'
 
 describe('modules/Auth/Auth.middleware', () => {
   describe('authLoginFlow', () => {
@@ -145,6 +153,137 @@ describe('modules/Auth/Auth.middleware', () => {
       await invoke(action)
 
       // then ... the login API should be called
+      expect(mockNotification).toHaveBeenCalled()
+    })
+  })
+  describe('authRegistrationFlow', () => {
+    it('should ignore other actions', async () => {
+      // given ... an action is fired
+      const create = createMiddlewareMock(jest)
+      const mockApi = jest.fn()
+      const action = { type: 'TEST', payload: {} }
+      // @ts-ignore
+      const { invoke, next } = create(SUT.authRegistrationFlow(mockApi))
+
+      // when ... we invoke another action
+      await invoke(action)
+
+      // then ... next should have been called with the action
+      expect(next).toHaveBeenCalledWith(action)
+      expect(mockApi).not.toHaveBeenCalled()
+    })
+
+    it('should correctly call the api with the given credentials', async () => {
+      // given ... the authRegistration action is fired
+      const create = createMiddlewareMock(jest)
+      const credentials = {
+        firstName: 'FIRST NAME',
+        lastName: 'LAST NAME',
+        email: 'USER EMAIL',
+        countryAlpha2: 'COUNTRY CODE',
+        password: 'USER PASSWORD',
+        confirmPassword: 'USER PASSWORD',
+        privacyInd: true,
+      }
+      const mockApi = { api: { auth: { register: jest.fn((_x: any) => Promise.resolve('RESOLVED VALUE')) } } }
+      const action = authRegistration(credentials)
+      // @ts-ignore
+      const { invoke, next } = create(SUT.authRegistrationFlow(mockApi))
+
+      // when ... we respond to the authRegistration action
+      await invoke(action)
+
+      // then ... the register API should be called
+      expect(next).toHaveBeenCalledWith(action)
+      expect(mockApi.api.auth.register).toHaveBeenCalled()
+    })
+
+    it('should correctly handle a successful registration', async () => {
+      // given ... the register api is called and returns successfully
+      const create = createMiddlewareMock(jest)
+      const credentials = {
+        firstName: 'FIRST NAME',
+        lastName: 'LAST NAME',
+        email: 'USER EMAIL',
+        countryAlpha2: 'COUNTRY CODE',
+        password: 'USER PASSWORD',
+        confirmPassword: 'USER PASSWORD',
+        privacyInd: true,
+      }
+      const response = defaultUserRegistrationResponseData
+      const mockApi = { api: { auth: { register: jest.fn((_x: any) => Promise.resolve(response)) } } }
+      const action = authRegistration(credentials)
+
+      // @ts-ignore
+      const { store, invoke } = create(SUT.authRegistrationFlow(mockApi))
+
+      // when ... we respond to the successful state
+      await invoke(action)
+
+      // then ... the success action should be dispatched with the response
+      expect(store.dispatch).toHaveBeenCalledWith(authRegistrationSuccess(response))
+    })
+    it('should correctly handle a failed registration', async () => {
+      // given ... the register api is called and returns a failure
+      const create = createMiddlewareMock(jest)
+      const credentials = {
+        firstName: 'FIRST NAME',
+        lastName: 'LAST NAME',
+        email: 'USER EMAIL',
+        countryAlpha2: 'COUNTRY CODE',
+        password: 'USER PASSWORD',
+        confirmPassword: 'USER PASSWORD',
+        privacyInd: true,
+      }
+      const response = 'ERROR: FAILED FOR A REASON'
+      const mockApi = { api: { auth: { register: jest.fn((_x: any) => Promise.reject(response)) } } }
+      const action = authRegistration(credentials)
+
+      // @ts-ignore
+      const { store, invoke } = create(SUT.authRegistrationFlow(mockApi))
+
+      // when ... we respond to the failure state
+      await invoke(action)
+
+      // then ... the failure action should be called with the error response
+      expect(store.dispatch).toHaveBeenCalledWith(authRegistrationFailure(response))
+    })
+  })
+
+  describe('authRegistrationSuccessFlow', () => {
+    it('should correctly send a notification to the user', async () => {
+      // given ... the authRegistration action is fired
+      const create = createMiddlewareMock(jest)
+      // @ts-ignore
+      const response = defaultUserRegistrationResponseData
+      const action = authRegistrationSuccess(response)
+      const mockNotification = jest.fn()
+      // @ts-ignore
+      const { invoke } = create(SUT.authRegistrationSuccessFlow({ notification: mockNotification }))
+
+      // when ... we respond to the authRegistration action
+      await invoke(action)
+
+      // then ... the registration API should be called
+      expect(mockNotification).toHaveBeenCalled()
+    })
+  })
+
+  describe('authRegistrationFailureFlow', () => {
+    it('should correctly send a notification to the user', async () => {
+      // given ... the authRegistration action is fired
+      const create = createMiddlewareMock(jest)
+      // @ts-ignore
+      const response = 'ERROR: FAILED FOR A REASON'
+      const action = authRegistrationFailure(response)
+      const mockNotification = jest.fn()
+      // @ts-ignore
+      const { invoke } = create(SUT.authRegistrationFailureFlow({ notification: mockNotification }))
+
+      // when ... we respond to the authRegistration action
+      await invoke(action)
+
+      // then ... the registration API should be called
       expect(mockNotification).toHaveBeenCalled()
     })
   })
