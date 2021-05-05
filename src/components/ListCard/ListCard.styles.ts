8751f287cb4a6c9bfec21e033cf811f2628a9694
sai.ranjit <sairanjit.tummalapalli@ayanworks.com>
2021-05-05 10:07:23 +0530

    feat(skills): adding count badge and list card component

---
+++ src/components/ListCard/ListCard.styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet, TextStyle, ViewStyle } from 'react-native'
+
+const styles = {
+  cardHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+  } as ViewStyle,
+  label: {
+    marginLeft: 10,
+  } as TextStyle,
+}
+
+export default StyleSheet.create(styles)
