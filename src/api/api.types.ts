ef6910d310c83141fd2b701728b49a23ead795fd
Cameron Olivier <cameronolivier@gmail.com>
2021-05-11 16:23:50 +0200

    feat(api): adding constants and utils for api middleware

--- src/api/api.types.ts
+++ src/api/api.types.ts
@@ -1,4 +1,48 @@
-import { AxiosRequestConfig, AxiosResponse } from 'axios'
+import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+import { ActionCreator } from 'redux'
 
 export type GenerateEndpoint = (arr: string[]) => string
-export type ApiClientFn = (body: Record<string, any>, config?: AxiosRequestConfig) => Promise<AxiosResponse>
+
+export type ApiEndpoints = string
+
+export enum ApiClients {
+  Auth = 'auth',
+  Users = 'users',
+  Organisations = 'organisations',
+  Skills = 'skills',
+  Jobs = 'jobs',
+}
+
+export enum ApiMethods {
+  Post = 'post',
+  Put = 'put',
+  Get = 'get',
+  Patch = 'patch',
+  Delete = 'delete',
+}
+
+export interface ApiClientMetaOverlap {
+  client: ApiClients
+  method: ApiMethods
+  endpoint: ApiEndpoints
+  params?: Record<string, string | number>
+  config?: Partial<AxiosRequestConfig>
+}
+
+export interface ApiClientArgs extends ApiClientMetaOverlap {
+  token?: string
+  data?: any
+}
+export type ApiClient = (args: ApiClientArgs) => Promise<AxiosResponse>
+export type CreateApiClient = (instance: AxiosInstance) => ApiClient
+export type ApiCall = CreateApiClient
+
+export interface ApiMeta extends ApiClientMetaOverlap {
+  isTokenRequired: boolean
+  onSuccess: ActionCreator<any>
+  onFailure: ActionCreator<any>
+}
+
+export type PrepareApiRequestData = Pick<ApiMeta, 'onSuccess' | 'onFailure'> & {
+  apiArgs: ApiClientArgs
+}
--- src/api/index.ts
+++ src/api/index.ts
@@ -2,13 +2,30 @@ import axios from 'axios'
 import { AUTH_TOKEN } from 'helpers/helpers'
 
 import Env from '../env.json'
+import * as apiConfig from './api.config'
+import { FALLBACK_MESSAGE, REFRESH_TOKEN_FAILED_MESSAGE, UNAUTHORIZED_STATUS_CODE } from './api.constants'
+import * as middleware from './api.middleware'
 import auth from './auth/auth'
 import digitalCv from './digitalCv'
 import users from './users'
 
-const FALLBACK_MESSAGE = 'Unable to reach server'
-const REFRESH_TOKEN_FAILED_MESSAGE = 'Unable to refresh your session'
-const UNAUTHORIZED_STATUS_CODE = 401
+// TODO:
+// - export an axios instance that takes an baseURL and initial headers.
+// - this needs to be able to take an access token, via a redux middleware call when it needs it.
+// - probably make use of a fp function so that we can pass auth token in at middleware time.
+// - so we want a function that takes an base (user, auth, etc) and a method (login, etc) and accepts the auth token
+// - we need to be able to build up the endpoints somehow
+// - will handle errors in middleware
+// - will handle getting / posting data through middleware
+
+// reconsidered shape:
+// middleware takes:
+// - the instance < - this is passed into the client on creation.
+// - the client
+// - the action
+// - a way to get auth (getAccessToken)
+// - we then call this. But I don't think we can pass the client or action in via redux/middleware. We need a function
+// - that takes the client and action as a string, and builds the call - and the client and action must be passed in via the action meta data
 
 const instance = axios.create({
   baseURL: Env.YOMA_API_BASE_PATH,
@@ -48,3 +65,5 @@ export default {
   users: users(instance),
   digitalCv: digitalCv(instance),
 }
+
+export { middleware, apiConfig }
