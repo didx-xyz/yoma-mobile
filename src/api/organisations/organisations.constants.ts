696e17d4680543619312f1c71fb2356372fcfe2a
Cameron Olivier <cameronolivier@gmail.com>
2021-05-11 23:07:15 +0200

    feat(api): adding skills, jobs, organisations

---
+++ src/api/organisations/organisations.constants.ts
@@ -0,0 +1,19 @@
+import { mergeRight } from 'ramda'
+
+import { ApiClients, ApiMeta, ApiMethods } from '../api.types'
+import { OrganisationsEndpoints } from './organisations.types'
+
+export const ORGANISATIONS_CONFIG: Partial<ApiMeta> = {
+  client: ApiClients.Organisations,
+  method: ApiMethods.Get,
+  isTokenRequired: false,
+}
+
+export const ORGANISATIONS_GET_ALL_CONFIG: Partial<ApiMeta> = mergeRight(ORGANISATIONS_CONFIG, {})
+export const ORGANISATIONS_GET_KEY_NAMES_CONFIG: Partial<ApiMeta> = mergeRight(ORGANISATIONS_CONFIG, {
+  endpoint: OrganisationsEndpoints.Names,
+})
+export const ORGANISATIONS_GET_BY_ID_CONFIG: Partial<ApiMeta> = mergeRight(ORGANISATIONS_CONFIG, {})
+export const ORGANISATIONS_CREATE_CONFIG: Partial<ApiMeta> = mergeRight(ORGANISATIONS_CONFIG, {
+  method: ApiMethods.Post,
+})
