c1a281aba8a39771e32b51a2143a493e3e8c76a0
Cameron Olivier <cameronolivier@gmail.com>
2021-05-13 21:33:16 +0200

    refactor(Style): currying applyAlphaToHex

--- src/styles/styles.utils.ts
+++ src/styles/styles.utils.ts
@@ -1,6 +1,7 @@
-import { join, multiply, of, pipe, prepend } from 'ramda'
+import { curry, join, multiply, of, pipe, prepend } from 'ramda'
 
 import { toFixed, toStringWithRadix } from '../utils/numbers.utils'
 
-export const applyAlphaToHex = (color: string) =>
-  pipe(multiply(256), toFixed(), toStringWithRadix(16), of, prepend(color), join(''))
+export const applyAlphaToHex = curry((color: string, alpha: number) =>
+  pipe(multiply(256), toFixed(), toStringWithRadix(16), of, prepend(color), join(''))(alpha),
+)
