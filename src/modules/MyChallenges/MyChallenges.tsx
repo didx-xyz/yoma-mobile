deda70e9450568de2df67327bb4eb3849f436e3b
sai.ranjit <sairanjit.tummalapalli@ayanworks.com>
2021-05-13 18:48:45 +0530

    refactor(InfoCard): adding height into constants

--- src/modules/MyChallenges/MyChallenges.tsx
+++ src/modules/MyChallenges/MyChallenges.tsx
@@ -7,6 +7,7 @@ import { HomeNavigatorParamsList } from 'modules/Home/Home.types'
 import React, { useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { FlatList, ScrollView } from 'react-native'
+import { CARD_SPACER } from 'styles/styles.constants'
 
 import { MOCKED_CHALLENGES } from './MyChallenges.constants'
 import styles from './MyChallenges.styles'
@@ -53,7 +54,7 @@ const MyChallenges = ({ navigation }: Props) => {
         fallback={
           <FlatList
             data={challenges}
-            ListHeaderComponent={<Spacer height={10} />}
+            ListHeaderComponent={<Spacer height={CARD_SPACER} />}
             renderItem={({ item }) => renderChallengeEntry(item)}
             keyExtractor={item => item.challenge}
           />
