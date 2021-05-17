830f0cbfa3758c34bf739299285b83468dda1e4f
sai.ranjit <sairanjit.tummalapalli@ayanworks.com>
2021-05-17 17:54:02 +0530

    refactor(DatePicker): refactored date picker into formik based component

--- src/components/DatePicker/DatePicker.styles.ts
+++ src/components/DatePicker/DatePicker.styles.ts
@@ -1,31 +1,19 @@
-import { StyleSheet, TextStyle, ViewStyle } from 'react-native'
+import { StyleSheet, ViewStyle } from 'react-native'
 import { Colors, colors } from 'styles'
 import { applyAlphaToHex } from 'styles/styles.utils'
-
-import textInputStyles from '../Input/Input.styles'
 
 const styles = {
   container: {
     flex: 1,
+    marginRight: 15,
   } as ViewStyle,
-  textInput: {
-    height: 40,
+  dateContainer: {
+    height: 30,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
     borderBottomWidth: 1,
     borderColor: applyAlphaToHex(colors[Colors.menuGrey])(0.7),
   } as ViewStyle,
-  dateInput: {
-    alignItems: 'flex-start',
-    borderWidth: 0,
-  } as ViewStyle,
-  dateText: {
-    ...textInputStyles.textInput,
-    borderBottomWidth: 0,
-  } as TextStyle,
-  placeholder: {
-    ...textInputStyles.textInput,
-    color: colors[Colors.menuGrey],
-    borderBottomWidth: 0,
-  } as TextStyle,
 }
 
 export default StyleSheet.create(styles)
--- src/modules/Education/EducationForm/EducationForm.tsx
+++ src/modules/Education/EducationForm/EducationForm.tsx
@@ -59,30 +59,8 @@ const EducationForm = forwardRef(({ navigation }: Props, ref) => {
             onPress={() => setIsStudying(!isStudying)}
           />
           <View style={styles.row}>
-            <DatePicker
-              onDateChange={(date: string) => {
-                formikHandlers.handleChange('startDate')
-                formikHandlers.handleBlur('startDate')
-                formikHandlers.setFieldValue('startDate', date)
-              }}
-              value={formikHandlers.values.startDate}
-              label={t('Start date')}
-              isTouched={formikHandlers.touched.startDate}
-              error={formikHandlers.errors.startDate}
-              showTitle={formikHandlers.values.startDate !== ''}
-            />
-            <DatePicker
-              onDateChange={(date: string) => {
-                formikHandlers.handleChange('endDate')
-                formikHandlers.handleBlur('endDate')
-                formikHandlers.setFieldValue('endDate', date)
-              }}
-              value={formikHandlers.values.endDate}
-              label={t('End date')}
-              isTouched={formikHandlers.touched.endDate}
-              error={formikHandlers.errors.endDate}
-              showTitle={formikHandlers.values.endDate !== ''}
-            />
+            <DatePicker name={'startDate'} label={t('Start date')} handlers={formikHandlers} />
+            <DatePicker name={'endDate'} label={t('End date')} handlers={formikHandlers} />
           </View>
           <Input name={'description'} label={t('Description')} handlers={formikHandlers} multiline />
           <DropDownTags
