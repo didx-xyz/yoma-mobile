90118b1a8247274dedb7cfd0f24fe736f719e166
seyi <sheyie2008@gmail.com>
2021-06-02 10:35:19 +0200

    refactor: wrapped navigator component with provider

--- index.tsx
+++ index.tsx
@@ -1,20 +1,12 @@
 import 'react-native-gesture-handler'
 import React from 'react'
 import { AppRegistry } from 'react-native'
-import { Provider } from 'react-redux'
-
-import { PersistGate } from 'redux-persist/integration/react'
 import { config } from './config'
 import App from './src/modules/App'
-import { store, persistor } from './src/redux/store'
 
 
 const Root = () => (
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
     <App />
-    </PersistGate>
-  </Provider>
 )
 
 AppRegistry.registerComponent(config.application.name, () => Root)
--- src/modules/App/App.tsx
+++ src/modules/App/App.tsx
@@ -1,25 +1,32 @@
-import { RNLocalize, setI18nConfig } from 'locales/i18n'
-import React, { useEffect } from 'react'
-import FlashMessage from 'react-native-flash-message'
+import { RNLocalize, setI18nConfig } from "locales/i18n";
+import React, { useEffect } from "react";
+import FlashMessage from "react-native-flash-message";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import AppNavigation from '../AppNavigation'
+import { persistor, store } from "../../redux/store";
+import AppNavigation from "../AppNavigation";
 
-setI18nConfig()
+setI18nConfig();
 
 const App = () => {
   useEffect(() => {
-    RNLocalize.addEventListener('change', setI18nConfig)
+    RNLocalize.addEventListener("change", setI18nConfig);
     return () => {
-      RNLocalize.removeEventListener('change', setI18nConfig)
-    }
-  }, [])
+      RNLocalize.removeEventListener("change", setI18nConfig);
+    };
+  }, []);
 
   return (
     <>
-      <AppNavigation />
-      <FlashMessage position="top" />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <AppNavigation />
+          <FlashMessage position="top" />
+        </PersistGate>
+      </Provider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
