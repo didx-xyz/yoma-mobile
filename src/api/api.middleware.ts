7b2099259e2436c637d9abf58d9e4df3faf5525e
Cameron Olivier <cameronolivier@gmail.com>
2021-06-09 00:41:04 +0200

    chore: fixing with prettier

--- src/api/api.middleware.ts
+++ src/api/api.middleware.ts
@@ -5,22 +5,26 @@ import { ApiClient } from './api.types'
 import { prepareApiRequest } from './api.utils'
 
 type Deps = { api: ApiClient; prepArgs: typeof prepareApiRequest }
-export const apiFlow = ({ api, prepArgs }: Deps): Middleware => ({ getState, dispatch }) => next => async action => {
-  const result = next(action)
+export const apiFlow =
+  ({ api, prepArgs }: Deps): Middleware =>
+  ({ getState, dispatch }) =>
+  next =>
+  async action => {
+    const result = next(action)
 
-  if (apiRequest.match(action)) {
-    const state = getState()
-    //@ts-ignore
-    const { onSuccess, onFailure, apiArgs } = prepArgs(state, action)
+    if (apiRequest.match(action)) {
+      const state = getState()
+      //@ts-ignore
+      const { onSuccess, onFailure, apiArgs } = prepArgs(state, action)
 
-    await api(apiArgs)
-      .then((response: any) => {
-        dispatch(onSuccess(response))
-      })
-      .catch((error: any) => {
-        dispatch(onFailure(error))
-      })
+      await api(apiArgs)
+        .then((response: any) => {
+          dispatch(onSuccess(response))
+        })
+        .catch((error: any) => {
+          dispatch(onFailure(error))
+        })
+    }
+
+    return result
   }
-
-  return result
-}
