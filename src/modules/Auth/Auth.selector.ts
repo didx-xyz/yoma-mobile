19949dfdf8772be0fe6c07af22545673cb8ac150
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-06-11 10:36:22 +0200

    refactor: Removed unused code

--- src/modules/Auth/Auth.selector.ts
+++ src/modules/Auth/Auth.selector.ts
@@ -2,9 +2,6 @@ import { equals, not, pathOr } from 'ramda'
 
 import { RootState } from './../../redux/redux.types'
 
-//TODO: use createSelector
-export const authSelector = pathOr(null, ['auth', 'user'])
-export const tokenSelector = pathOr('', ['auth', 'token'])
 export const isAuthenticatedSelector = (state: RootState) => not(equals(pathOr(false, ['auth', 'token'], state), ''))
 
-export default { authSelector, tokenSelector, isAuthenticatedSelector }
+export default { isAuthenticatedSelector }
