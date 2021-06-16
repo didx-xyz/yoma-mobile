4235931f77368f74cbcc8ae772473719d603892d
Cameron Olivier <cameronolivier@gmail.com>
2021-06-16 15:25:44 +0200

    YOMA-292 -  refactor(Auth): updating login to use api module (#67)
    * refactor(Auth): reworking login to use auth middleware
    
    * refactor(Auth): getting error messages sending correctly
    
    * refactor(Auth): getting success data into state
    
    * chore: adding todos
    
    * refactor(Auth): using api middleware to login
    
    * refactor(Auth): using api middleware to login

--- src/api/api.reducer.ts
+++ src/api/api.reducer.ts
@@ -1,16 +1,13 @@
 import { createAction } from '@reduxjs/toolkit'
 
-import { StdObj } from '../types/general.types'
 import { ApiMeta } from './api.types'
 
 const name = '[api]'
 type ApiRequestResponse = {
-  payload: StdObj | undefined
+  payload: any | undefined
   meta: ApiMeta
 }
 export const apiRequest = createAction(
   `${name} apiRequest`,
-  (meta: ApiMeta, payload?: StdObj): ApiRequestResponse => ({ payload, meta }),
+  (meta: ApiMeta, payload?: any): ApiRequestResponse => ({ payload, meta }),
 )
-export const apiRequestSuccess = createAction<any>(`${name} apiRequestSuccess`)
-export const apiRequestFailure = createAction<any>(`${name} apiRequestFailure`)
