53cfc97a491c1a321b20ed3ba31e48abaad4a2f7
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-06-17 09:58:39 +0200

    refactor: adjustments as per PR comments

--- src/modules/Auth/Auth.selector.ts
+++ src/modules/Auth/Auth.selector.ts
@@ -1,7 +1,7 @@
 import { createSelector } from '@reduxjs/toolkit'
-import { equals, not, propOr } from 'ramda'
+import { isNil, complement, propOr } from 'ramda'
 import { RootState } from 'redux/redux.types'
 
 export const selectAuthState = (state: Partial<RootState>) => state.auth
 export const selectToken = createSelector(selectAuthState, propOr(null, 'token'))
-export const selectIsAuthenticated = createSelector(selectToken, token => equals(not(token), false))
+export const selectIsAuthenticated = createSelector(selectToken, complement(isNil))
