a788dd5f521d1875f5cf1fcb128d517538895880
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-06-24 22:56:57 +0200

    chore: fixed conflicts

--- src/modules/App/App.middleware.ts
+++ src/modules/App/App.middleware.ts
@@ -3,12 +3,15 @@ import { Middleware } from 'redux'
 import * as AuthActions from './../Auth/Auth.reducer'
 import { resetAppData } from './App.reducer'
 
-export const appResetFlow: Middleware = ({ dispatch }) => next => action => {
-  const result = next(action)
+export const appResetFlow: Middleware =
+  ({ dispatch }) =>
+  next =>
+  action => {
+    const result = next(action)
 
-  if (resetAppData.match(action)) {
-    dispatch(AuthActions.clearAuth())
+    if (resetAppData.match(action)) {
+      dispatch(AuthActions.clearAuth())
+    }
+
+    return result
   }
-
-  return result
-}
