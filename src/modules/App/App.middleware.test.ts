b404ca4994dc5ec6bd3bcc760de7fc2f13309ecb
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-06-25 00:55:13 +0200

    tests: app middleware

---
+++ src/modules/App/App.middleware.test.ts
@@ -0,0 +1,30 @@
+import { createMiddlewareMock } from '../../../tests/tests.utils'
+import * as SUT from './App.middleware'
+import { resetAppData } from './App.reducer'
+
+describe('modules/App/App.middleware', () => {
+  describe('appResetFlow', () => {
+    it('should correctly handle an app reset action', () => {
+      const create = createMiddlewareMock(jest)
+      // given ... the resetAppData action is fired
+      const action = resetAppData()
+      const { store, next, invoke } = create(SUT.appResetFlow)
+
+      invoke(action)
+      // ... we validate that our actions were triggered
+      expect(next).toHaveBeenCalledWith(action)
+      expect(store.dispatch).toHaveBeenCalled()
+    })
+    it('should handle app reset action accurately', () => {
+      const create = createMiddlewareMock(jest)
+
+      // given ... the resetAppData action is fired
+      const action = resetAppData()
+      const { store, invoke } = create(SUT.appResetFlow)
+
+      invoke(action)
+      // ... we validate that our actions were triggered
+      expect(store.dispatch).toBeCalled()
+    })
+  })
+})
