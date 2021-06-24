3872e7a64d3c122b5ef4b702480cac99e2ccd534
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-06-24 23:13:22 +0200

    refactor: changes as per PR comments

--- src/modules/Auth/Auth.utils.ts
+++ src/modules/Auth/Auth.utils.ts
@@ -2,4 +2,4 @@ import { path, pathOr, pick, pipe } from 'ramda'
 
 export const selectCredentialsFromLoginPayload = pipe(path(['payload', 'data', 'data']), pick(['token', 'expiresAt']))
 export const selectRefreshTokenFromLoginPayload = pathOr('', ['payload', 'data', 'data', 'refreshToken'])
-export const selectLoginCredentialsFromRegistration = pipe(pick(['email', 'password']))
+export const selectLoginCredentialsFromRegistration = pick(['email', 'password'])
