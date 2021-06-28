2d32cad5349f3a92932a2e515d1a1532c3e9c679
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-06-28 06:00:12 +0200

    refactor: setup user reducer

--- src/modules/User/User.reducer.ts
+++ src/modules/User/User.reducer.ts
@@ -1,10 +1,13 @@
 import { createAction, createReducer } from '@reduxjs/toolkit'
+import { mergeDeepRight } from 'ramda'
 
 const name = '[User]'
 export const INITIAL_STATE = {}
 
 export const User = createAction(`${name}`)
 
-const UserReducer = createReducer(INITIAL_STATE, builder => {})
+const UserReducer = createReducer(INITIAL_STATE, builder => {
+  builder.addCase(User, (state, action) => mergeDeepRight(state)(action.payload))
+})
 
 export default UserReducer
