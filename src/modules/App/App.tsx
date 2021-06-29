00a8bb811c0eaa041ec26ce8a08ea85a9d318531
Cameron Olivier <cameronolivier@gmail.com>
2021-06-30 00:01:03 +0200

    refactor(App): fixing Navigation imprt in the App component

--- src/modules/App/App.tsx
+++ src/modules/App/App.tsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistor, store } from 'redux/store'
 
-import Navigation from '../Navigation/Navigation.container'
+import Navigation from '../Navigation'
 
 setI18nConfig()
 
