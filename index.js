03b06fb9be225472f15a8fad9a30bafa63ed7527
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-07-05 07:43:08 +0200

    refactor: PR comment changes

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ import React from 'react'
 import { AppRegistry } from 'react-native'
 import 'react-native-gesture-handler'
 import { config } from './config'
-import App from './src/modules/App/App'
+import App from './src/modules/App'
+import { persistor, store } from './src/redux/store'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-const Root = () => (<App />)
+const Root = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+)
 
 AppRegistry.registerComponent(config.application.name, () => Root)
--- src/modules/App/index.ts
+++ src/modules/App/index.ts
@@ -1,4 +1,6 @@
+import App from './App'
 import * as middleware from './App.middleware'
 import * as actions from './App.reducer'
 
 export { actions, middleware }
+export default App
