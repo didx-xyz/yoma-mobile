b0ccd56eec860cf856571fff7850f5cf0707cdc3
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-07-06 21:12:36 +0200

    refactor: fix conflicts

--- src/modules/User/User.selector.test.ts
+++ src/modules/User/User.selector.test.ts
@@ -32,45 +32,6 @@ describe('modules/User/User.selector', () => {
       expect(result).toEqual('BIOGRAPHY')
     })
   })
-  describe('selectUserUpdateCredentials', () => {
-    it('should return user data merged with update payload', () => {
-      // given ...
-      const mockState = {
-        user: {
-          id: 'USER_ID',
-          firstName: 'FIRST_NAME',
-          lastName: 'LAST_NAME',
-          phoneNumber: 'PHONE_NUMBER',
-          biography: '',
-          countryAlpha2: 'COUNTRY_ALPHA2',
-          email: 'USER_EMAIL@SOMEWHERE.TEST',
-          zltoWalletId: 'ZLTO_WALLET_ID',
-          zltoBalance: 1000,
-          covidChallengeCertificateURL: 'COVID_CHALLENGE_CERTIFICATE_URL',
-          tideChallengeCertificateURL: 'TIDE_CHALLENGE_CERTIFICATE_URL',
-          photoURL: 'PHOTO_URL',
-          role: 'ROLE',
-          organisation: 'ORGANISATION',
-          createdAt: 'CREATED_AT',
-          lastLogin: 'LAST_LOGIN',
-        },
-      }
-      const mockPayload = {
-        payload: {
-          biography: 'BIOGRAPHY',
-        },
-      }
-      // when selectUserUpdateCredentials
-      const result = SUT.selectUserUpdate(mockPayload)(mockState)
-
-      //then expect user request data
-      expect(result).toEqual({
-        biography: 'BIOGRAPHY',
-        firstName: 'FIRST_NAME',
-        lastName: 'LAST_NAME',
-        phoneNumber: 'PHONE_NUMBER',
-        countryAlpha2: 'COUNTRY_ALPHA2',
-      })
   describe('selectUser ', () => {
     it('should return user property of the root state', () => {
       const state = {
