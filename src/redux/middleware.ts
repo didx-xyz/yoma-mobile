1a0370045c145661d0c46dfb51557065632f972f
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-07-06 08:30:33 +0200

    YOMA-480 - refactor(App): Error running app: Module AppRegistry is not a registered callable module (#87)
    * refactor: fixed build and crashing issues
    
    * refactor: PR comment changes
    
    * fix(Auth): adding _state and _action arguments to Clear auth reducer -seems to fix the slow ui issues
    
    * chore: bumping version code
    
    * chore: removing whitespace
    
    * chore: testing build on firebase app distro
    
    * chore: removing branch from FAD deploy
    
    * chore: updating release notes
    
    Co-authored-by: Cameron Olivier <cameronolivier@gmail.com>

--- src/redux/middleware.ts
+++ src/redux/middleware.ts
@@ -1,10 +1,10 @@
 import { setItemAsync } from 'expo-secure-store'
+import { middleware as appMiddleware } from 'modules/App'
 import { concat } from 'ramda'
 import { Middleware } from 'redux'
 
 import { apiConfig, middleware as apiMiddleware } from '../api'
 import { prepareApiRequest } from '../api/api.utils'
-import { appMiddleware } from '../modules/App'
 import { middleware as authMiddleware } from '../modules/Auth'
 import { showSimpleMessage } from '../utils/error'
 
