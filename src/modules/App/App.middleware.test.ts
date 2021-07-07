6f7510700fb45d6040a6a6e18d0929f4af08f74b
Cameron Olivier <cameronolivier@gmail.com>
2021-07-07 23:18:03 +0200

    tests: huge fights getting this to work... but working now. Had to refactor the way we're passing the FB/Google svg as the test suite was moaning.

--- src/modules/App/App.middleware.test.ts
+++ src/modules/App/App.middleware.test.ts
@@ -4,6 +4,35 @@ import { actions as UserActions } from '../User'
 import * as SUT from './App.middleware'
 import { fetchInitial, resetAppData } from './App.reducer'
 
+jest.mock('react-native-localize', () => ({
+  getLocales: () => [
+    { countryCode: 'GB', languageTag: 'en-GB', languageCode: 'en', isRTL: false },
+    { countryCode: 'US', languageTag: 'en-US', languageCode: 'en', isRTL: false },
+    { countryCode: 'FR', languageTag: 'fr-FR', languageCode: 'fr', isRTL: false },
+  ],
+
+  getNumberFormatSettings: () => ({
+    decimalSeparator: '.',
+    groupingSeparator: ',',
+  }),
+
+  getCalendar: () => 'gregorian', // or "japanese", "buddhist"
+  getCountry: () => 'US', // the country code you want
+  getCurrencies: () => ['USD', 'EUR'], // can be empty array
+  getTemperatureUnit: () => 'celsius', // or "fahrenheit"
+  getTimeZone: () => 'Europe/Paris', // the timezone you want
+  uses24HourClock: () => true,
+  usesMetricSystem: () => true,
+
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  findBestAvailableLanguage: () => ({
+    languageTag: 'en-US',
+    isRTL: false,
+  }),
+}))
+jest.mock('react-native/Libraries/LogBox/LogBox')
+
 describe('modules/App/App.middleware', () => {
   describe('appResetFlow', () => {
     it('should correctly handle an app reset action', () => {
