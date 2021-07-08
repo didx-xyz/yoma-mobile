8c979010f48f331fce1bc3b4c5ba86631ac78daa
Cameron Olivier <cameronolivier@gmail.com>
2021-07-08 22:40:16 +0200

    tests: adding tests for selectUserId, and fixing rootStateFixture util

--- src/modules/User/User.selector.test.ts
+++ src/modules/User/User.selector.test.ts
@@ -1,24 +1,46 @@
+import { rootStateFixture } from '../../redux/redux.test.fixtures'
 import { INITIAL_STATE } from '../User/User.reducer'
 import { USER_RESPONSE } from './../Profile/Profile.constants'
 import * as SUT from './User.selector'
+import { selectUserId } from './User.selector'
 
 describe('modules/User/User.selector', () => {
   describe('selectUser ', () => {
     it('should return user property of the root state', () => {
-      const state = {
+      const stateMock = rootStateFixture({
         user: USER_RESPONSE,
-      }
+      })
       // when ... we call the selector
-      const result = SUT.selectUser(state)
+      const result = SUT.selectUser(stateMock)
       // then ... should return result as expected
-      expect(result).toEqual(state.user)
+      expect(result).toEqual(stateMock.user)
     })
     it('should return the default user state', () => {
-      const state = { user: INITIAL_STATE }
+      const stateMock = rootStateFixture()
       // when ... we call the selector
-      const result = SUT.selectUser(state)
+      const result = SUT.selectUser(stateMock)
       // then ... should return result as expected
-      expect(result).toEqual(state.user)
+      expect(result).toEqual(stateMock.user)
+    })
+  })
+  describe('selectUserId ', () => {
+    it('should return the default value for the id if none exists', () => {
+      // given ... no user data available
+      const stateMock = rootStateFixture()
+      // when ... we get the user's id
+      const result = SUT.selectUserId(stateMock)
+      // then ... should return an empty string
+      expect(result).toBe('')
+    })
+    it('should return user property of the root state', () => {
+      // given ... a valid user
+      const stateMock = rootStateFixture({
+        user: { ...USER_RESPONSE, id: 'A USER ID' },
+      })
+      // when ... we get the user's id
+      const result = SUT.selectUserId(stateMock)
+      // then ... we should the user's id returned
+      expect(result).toBe('A USER ID')
     })
   })
 })
