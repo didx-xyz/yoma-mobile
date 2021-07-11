73fe06574c0d2c23be57f335106963511e7d17c7
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-07-11 09:12:44 +0200

    refactor: restored social icons

--- src/components/SocialButton/SocialButton.constants.tsx
+++ src/components/SocialButton/SocialButton.constants.tsx
@@ -1,15 +1,18 @@
+import React from 'react'
+
+import { FacebookIcon, GoogleIcon } from '../../assets/images'
 import { Colors } from '../../styles'
 import { SocialVariants } from './SocialButton.types'
 
 export const MAP_VARIANT_TO_OPTIONS = {
   [SocialVariants.Facebook]: {
     color: Colors.white,
-    icon: 'FacebookIcon',
+    icon: <FacebookIcon />,
     translationKey: 'loginFacebook',
   },
   [SocialVariants.Google]: {
     color: Colors.primaryDarkGrey,
-    icon: 'GoogleIcon',
+    icon: <GoogleIcon />,
     translationKey: 'loginGoogle',
   },
 }
