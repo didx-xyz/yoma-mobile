886d861c136a498676e30b77b74218837944172a
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-07-15 04:20:42 +0200

    refactor: removed console.log

--- src/api/api.utils.ts
+++ src/api/api.utils.ts
@@ -34,7 +34,7 @@ export const addParamsToConfig = addValueWithGivenKeyToConfig('params')
 export const createParam = objOf
 export const createTypeParam = createParam('type')
 
-export const generateSanitisedEndpoint = pipe(flatten, filter(complement(isNil)), join('/'), console.log)
+export const generateSanitisedEndpoint = pipe(flatten, filter(complement(isNil)), join('/'))
 export const addHeaders = (headers: StdObj<string>) => pipe(concat([headers]), mergeAll)
 export const setAuthTokenHeader = unless(isNil, pipe(concat('Bearer '), objOf('Authorization')))
 export const getTokenFromState = pathOr(null, ['auth', 'token'])
