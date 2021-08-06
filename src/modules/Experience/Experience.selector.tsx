9734302f990d50bdbb28a29fc740b912bac8d7a3
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-08-06 09:38:51 +0200

    refactor: added skills to experience

---
+++ src/modules/Experience/Experience.selector.tsx
@@ -0,0 +1,7 @@
+import 'modules/Skills/Skills.selector'
+import { selectFilteredSkills } from 'modules/Skills/Skills.selector'
+import { createSelector } from 'reselect'
+
+export default createSelector(selectFilteredSkills, filteredSkills => ({
+  skills: filteredSkills,
+}))
