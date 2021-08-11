f08a9770aaf80efdcb70209489e94b46b8735732
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-08-11 09:50:49 +0200

    test: added test data

--- src/modules/Experience/Experience.selector.test.tsx
+++ src/modules/Experience/Experience.selector.test.tsx
@@ -5,14 +5,23 @@ import * as SUT from './Experience.selector'
 describe('modules/Experience/Experience.selector', () => {
   describe('selector', () => {
     it('should return organisations from the root state', () => {
-      const state = rootStateFixture()
+      const state = rootStateFixture({
+        organisations: [
+          {
+            key: 'A KEY',
+            value: 'SOME VALUE',
+          },
+        ],
+      })
       // when ... we call the selector
       const result = SUT.default(state)
       // then ... should return result as expected
-      const { organisations } = result
-      expect(result).toEqual({
-        organisations,
-      })
+      expect(result.organisations).toEqual([
+        {
+          key: 'A KEY',
+          value: 'SOME VALUE',
+        },
+      ])
     })
   })
 })
