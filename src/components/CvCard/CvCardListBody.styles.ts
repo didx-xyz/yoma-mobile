14c8009ac72ef3a8572e712f18f0b36e63ac5bcb
Cameron Olivier <cameronolivier@gmail.com>
2021-08-12 01:00:38 +0200

    chore: putting styles on separate lines for CvCardListBody

--- src/components/CvCard/CvCardListBody.styles.ts
+++ src/components/CvCard/CvCardListBody.styles.ts
@@ -1,7 +1,10 @@
 import { StyleSheet, ViewStyle } from 'react-native'
 
 const styles = {
-  container: { flex: 1, width: '100%' } as ViewStyle,
+  container: {
+    flex: 1,
+    width: '100%',
+  } as ViewStyle,
 }
 
 export default StyleSheet.create(styles)
