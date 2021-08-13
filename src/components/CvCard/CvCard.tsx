27fda4b4fcf83d3f2df2dd5f8c5534e931ef52ad
Cameron Olivier <cameronolivier@gmail.com>
2021-08-13 08:54:25 +0200

    refactor(CvCard): moving sub-components into their own directories, adjusting data type to accept normalised data

--- src/components/CvCard/CvCard.tsx
+++ src/components/CvCard/CvCard.tsx
@@ -4,11 +4,11 @@ import { View } from 'react-native'
 import { Colors } from '../../styles'
 import { WithChildren } from '../../types/react.types'
 import { isNotNil } from '../../utils/ramda.utils'
+import EditButton from '../EditButton/EditButton'
 import ListCardHeader from '../ListCardHeader'
 import Optional from '../Optional'
 import Text, { TextAlign } from '../Typography'
 import styles from './CvCard.styles'
-import EditButton from './EditButton'
 
 type Props = WithChildren<{
   title: string
--- src/components/CvCard/CvCardListBody/CvCardListBody.tsx
+++ src/components/CvCard/CvCardListBody/CvCardListBody.tsx
@@ -2,30 +2,33 @@ import { slice } from 'ramda'
 import React from 'react'
 import { View } from 'react-native'
 
-import Button, { ButtonVariants } from '../Button'
-import Divider from '../Divider'
-import { Optional } from '../index'
+import { NormalisedData } from '../../../redux/redux.types'
+import Button, { ButtonVariants } from '../../Button'
+import Divider from '../../Divider'
+import { Optional } from '../../index'
 import styles from './CvCardListBody.styles'
 
 interface Props {
-  data: any[]
+  data: NormalisedData
   onViewAll: () => void
   Item: any
   maxDisplay?: number
 }
+
 const CvCardListBody = ({ data, onViewAll, Item, maxDisplay = 2 }: Props) => {
-  const dataToDisplay: typeof data = slice(0, maxDisplay, data)
+  const { ids, entities } = data
+  const dataIdsToDisplay: typeof ids = slice(0, maxDisplay, ids)
   return (
     <View style={styles.container}>
-      {dataToDisplay.map((item, index) => (
+      {dataIdsToDisplay.map((item, index) => (
         <View>
-          <Item {...item} />
-          <Optional condition={index !== dataToDisplay.length - 1}>
+          <Item key={item || index} {...entities[item]} />
+          <Optional condition={index !== dataIdsToDisplay.length - 1}>
             <Divider />
           </Optional>
         </View>
       ))}
-      <Optional condition={data.length > dataToDisplay.length}>
+      <Optional condition={ids.length > dataIdsToDisplay.length}>
         <Divider />
         <Button label="View All" variant={ButtonVariants.Clear} onPress={onViewAll} />
       </Optional>
