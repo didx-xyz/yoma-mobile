2c2f9c1db705c5421bd142796a5a48d6c38a09ef
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-08-13 13:39:50 +0200

    refactor: removed unused selector

--- src/modules/Organisations/index.ts
+++ src/modules/Organisations/index.ts
@@ -1,5 +1,4 @@
 import * as middleware from './Organisations.middleware'
 import reducer, * as actions from './Organisations.reducer'
-import * as selector from './Organisations.selector'
 
-export { actions, middleware, reducer, selector }
+export { actions, middleware, reducer }
