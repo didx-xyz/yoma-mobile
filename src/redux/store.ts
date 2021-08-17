1926947ecd60291ce0294835fb4a3c58392c4c20
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-08-17 08:26:12 +0200

    refactor: removed comment on persistConfig

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -11,7 +11,6 @@ const persistConfig = {
   key: 'root',
   version: 1,
   storage: AsyncStorage,
-  // blackList: ['credentialItems'],
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
