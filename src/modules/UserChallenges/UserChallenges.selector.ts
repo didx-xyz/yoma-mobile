077fed5f8234b044eafd839ab0203a201931ad74
Cameron Olivier <cameronolivier@gmail.com>
2021-08-19 00:46:07 +0200

    refactor(UserChallengesWidget): creating selector for widget data

--- src/modules/UserChallenges/UserChallenges.selector.ts
+++ src/modules/UserChallenges/UserChallenges.selector.ts
@@ -1,23 +1,3 @@
-import { createSelector } from '@reduxjs/toolkit'
-import { applySpec, evolve, map, objOf, path, pathOr, pipe, propOr } from 'ramda'
-
 import { RootState } from '../../redux/redux.types'
 
 export const selectUserChallenges = (state: RootState) => state.userChallenges
-
-export default createSelector(
-  selectUserChallenges,
-  pipe(
-    evolve({
-      entities: map(
-        applySpec({
-          name: pathOr('', ['challenge', 'name']),
-          startDate: propOr('', 'startDate'),
-          avatarUrl: path(['challenge', 'url']),
-          isValidated: propOr(false, 'approved'),
-        }),
-      ),
-    }),
-    objOf('challenges'),
-  ),
-)
--- src/modules/UserChallenges/Widget/UserChallengesWidget.container.tsx
+++ src/modules/UserChallenges/Widget/UserChallengesWidget.container.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-import selector from '../UserChallenges.selector'
 import UserChallengesWidget from './UserChallengesWidget'
+import selector from './UserChallengesWidget.selector'
 
 const UserChallengesWidgetContainer = () => {
   const { challenges } = useSelector(selector)
