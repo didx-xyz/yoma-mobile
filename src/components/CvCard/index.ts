ea6e3c8e67c0b41dc808770341ffad20966ae1f1
Cameron Olivier <cameronolivier@gmail.com>
2021-08-18 23:52:03 +0200

    chore: bumping version, fixing cvcard import

--- src/components/CvCard/index.ts
+++ src/components/CvCard/index.ts
@@ -1,5 +1,5 @@
 import CvCard from './CvCard'
-import CvCardListBody from './CvCardListBody/CvCardListBody'
+import CvCardListBody from './CvCardListBody'
 
 export default CvCard
 export { CvCardListBody }
