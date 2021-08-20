0c9eff442b537c2d52c154dd04832f8b443cd2a2
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-08-20 11:30:13 +0200

    refactor: added test for updateNormalisedUserJobs

--- src/modules/UserJobs/UserJobs.reducer.test.ts
+++ src/modules/UserJobs/UserJobs.reducer.test.ts
@@ -1,4 +1,4 @@
-import SUT, { clearUserJobs, INITIAL_STATE, setUserJobs } from './UserJobs.reducer'
+import SUT, { clearUserJobs, INITIAL_STATE, setUserJobs, updateNormalisedUserJobs } from './UserJobs.reducer'
 
 describe('src/modules/User/Jobs/Jobs.reducer', () => {
   describe('setUserJobs', () => {
@@ -49,6 +49,61 @@ describe('src/modules/User/Jobs/Jobs.reducer', () => {
 
       // then ... state should include the new jobs
       expect(result).toEqual(jobsMock)
+    })
+  })
+  describe('updateNormalisedUserJobs', () => {
+    it('should correctly update the userJobs state', () => {
+      // given ...an initial state
+      const state = INITIAL_STATE
+
+      // when ... we add new jobs
+      const jobsMock = {
+        ids: ['id1', 'id2', 'id3'],
+        entities: {
+          id1: 'Job 1',
+          id2: 'Job 2',
+          id3: 'Job 3',
+        },
+        formValues: null,
+      }
+      // @ts-ignore
+      const action = updateNormalisedUserJobs(jobsMock)
+      const result = SUT(state, action)
+
+      // then ... state should include the new jobs
+      expect(result).toEqual(jobsMock)
+    })
+    it('should merge updated values with normalised state', () => {
+      // given ...an initial state
+      const state = {
+        ids: ['idA', 'idB'],
+        entities: {
+          idA: 'Job A',
+          idB: 'Job B',
+        },
+      }
+
+      // when ... we add new jobs
+      const jobsMock = {
+        ids: ['idC'],
+        entities: {
+          idC: 'Job C',
+        },
+      }
+      // @ts-ignore - the shape of the data doesn't matter
+      const action = updateNormalisedUserJobs(jobsMock)
+      // @ts-ignore - the shape of the data doesn't matter
+      const result = SUT(state, action)
+
+      // then ... state should include the new jobs
+      expect(result).toEqual({
+        ids: ['idA', 'idB', 'idC'],
+        entities: {
+          idA: 'Job A',
+          idB: 'Job B',
+          idC: 'Job C',
+        },
+      })
     })
   })
   describe('clearUserJobs', () => {
