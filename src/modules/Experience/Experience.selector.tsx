d1ad7a4710538078733e2e38bb9382e49045d48d
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-08-22 17:27:19 +0200

    refactor: removed experience folder

--- src/modules/Experience/Experience.selector.tsx
+++
@@ -1,6 +0,0 @@
-import { selectJobEntities } from 'modules/Job/Job.selector'
-import { createSelector } from 'reselect'
-
-export default createSelector(selectJobEntities, jobEntities => ({
-  jobs: jobEntities,
-}))
