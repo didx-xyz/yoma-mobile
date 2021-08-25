5a19e2216828c238c22a24394807384236359186
Seyi  Ogunbowale <sheyie2008@gmail.com>
2021-08-25 13:19:00 +0200

    refactor: refactored selectFilterSkillsByTerm util

--- src/modules/Skills/Skills.utils.test.tsx
+++ src/modules/Skills/Skills.utils.test.tsx
@@ -1,6 +1,53 @@
+import { keys, repeat, zipObj } from 'ramda'
+
 import * as SUT from './Skills.utils'
 
 describe('modules/Skills/Skills.utils', () => {
+  describe('selectFilterSkillsByTerm', () => {
+    it('should extract skill name from entities', () => {
+      //given ... actual state shape
+      const mockState = {
+        searchTerm: 'value1',
+        ids: ['key1', 'key2'],
+        entities: {
+          key1: {
+            key: 'key1',
+            value: 'value1',
+          },
+          key2: {
+            key: 'key2',
+            value: 'value2',
+          },
+        },
+      }
+
+      //when .. selectFiltered
+      const result = SUT.selectFilterSkillsByTerm(mockState)
+
+      //then
+      expect(result).toEqual([
+        {
+          key: 'key1',
+          value: 'value1',
+        },
+      ])
+    })
+    it('should return 20 skills if value is empty', () => {
+      //given ...mocked state with 30 skills
+      const ids = keys(repeat(0, 30))
+      const mockState = {
+        searchTerm: '',
+        ids,
+        entities: zipObj(ids, repeat({ key: 'key', value: 'skill' }, 30)),
+      }
+
+      // when .. selectFiltered with empty value
+      const result = SUT.selectFilterSkillsByTerm(mockState)
+
+      //then .. return 20 skills
+      expect(result).toHaveLength(20)
+    })
+  })
   describe('updateStateWithSearchTerm', () => {
     it('should update skills state with search term', () => {
       //given ...
--- src/modules/Skills/Skills.utils.tsx
+++ src/modules/Skills/Skills.utils.tsx
@@ -1,15 +1,18 @@
-import { curry, filter, mergeRight, pipe, slice, values } from 'ramda'
+import { curry, filter, map, mergeRight, pipe, slice, values } from 'ramda'
 
+import { FALLBACK_SKILLS_LENGTH, SEARCH_TERM_MIN_LENGTH } from './Skills.constants'
 import { SkillsState } from './Skills.types'
 
-//filter entity by search term else fallback to 20 records as default
-export const selectFilterSkillsByTerm = curry(({ searchTerm = '', entities }: SkillsState, minTermLength = 3) => {
-  const filtered = pipe(
-    values,
-    filter((obj: any) => obj.value.indexOf(searchTerm) === 0),
-  )(entities)
+export const selectFilterSkillsByTerm = curry(({ searchTerm = '', entities, ids }: SkillsState) => {
+  if (searchTerm.length < SEARCH_TERM_MIN_LENGTH) {
+    const defaultIds = slice(0, FALLBACK_SKILLS_LENGTH, ids)
+    return map((id: string | number) => entities[id].value, defaultIds)
+  }
 
-  return searchTerm.length < minTermLength ? slice(0, 20, filtered) : filtered
+  return pipe(
+    filter((obj: any) => obj.value.indexOf(searchTerm) === 0),
+    values,
+  )(entities)
 })
 
 export const updateStateWithSearchTerm = (state: SkillsState, searchTerm: string) => mergeRight(state, { searchTerm })
