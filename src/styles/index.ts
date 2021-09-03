47bcd3a7053c8c0bfe0b20238254b2be33f2fa24
Cameron Olivier <cameronolivier@gmail.com>
2021-09-03 09:55:57 +0200

    chore: removing unused files

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,4 @@
 import colors from './colors.styles'
 import { Colors, FontFamily } from './styles.types'
-import TextStyles from './text.styles'
 
-export { colors, Colors, TextStyles, FontFamily }
+export { colors, Colors, FontFamily }
