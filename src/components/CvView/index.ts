a040401d0de53714fe5f6fe4709aa23966ae18b2
Cameron Olivier <cameronolivier@gmail.com>
2021-09-08 23:32:53 +0200

    refactor(CvView): removing CvViewCredential from imports/exports

--- src/components/CvView/index.ts
+++ src/components/CvView/index.ts
@@ -1,6 +1,5 @@
-import CvViewCredential, { types as CvViewCredentialTypes } from '../CvViewCredential'
 import CvView from './CvView'
 import CvViewList from './List'
 
 export default CvView
-export { CvViewList, CvViewCredential, CvViewCredentialTypes }
+export { CvViewList }
