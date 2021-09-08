29696bc48d18611b3700b7324749b8e19fc8e6a1
Cameron Olivier <cameronolivier@gmail.com>
2021-09-08 23:35:30 +0200

    chore: removing unused imports/exports

--- src/components/CvWidget/index.ts
+++ src/components/CvWidget/index.ts
@@ -1,6 +1,5 @@
-import * as types from '../CvWidgetCredential/CvWidgetCredential.types'
 import CvWidget from './CvWidget'
 import CvWidgetList from './List'
 
 export default CvWidget
-export { CvWidgetList, types }
+export { CvWidgetList }
