b3c4bfbf5b82e122596cbb8eb9aa091020de968f
Cameron Olivier <cameronolivier@gmail.com>
2021-09-08 23:33:56 +0200

    chore: exporting module for types as Types

--- src/components/CvViewCredential/CvViewCredential.types.ts
+++ src/components/CvViewCredential/CvViewCredential.types.ts
@@ -1,9 +1,9 @@
 import React from 'react'
 
-import { StdObj } from '../../types/general.types'
+import * as Types from '../../types/general.types'
 import CvViewCredential from './index'
 
 export interface CvViewCredentialsData {
   ids: string[]
-  entities: StdObj<Omit<React.ComponentProps<typeof CvViewCredential>, 'onEdit'>>
+  entities: Types.StdObj<Omit<React.ComponentProps<typeof CvViewCredential>, 'onEdit'>>
 }
