9ac242525495d8eee7e94e11f2b5d822a496cd98
Cameron Olivier <cameronolivier@gmail.com>
2021-10-08 00:38:38 +0200

    feat(Education): adding selector for orgs and skills

--- src/modules/Skills/index.ts
+++ src/modules/Skills/index.ts
@@ -1,4 +1,5 @@
 import * as middleware from './Skills.middleware'
 import reducer, * as actions from './Skills.reducer'
+import * as selectors from './Skills.selector'
 
-export { actions, middleware, reducer }
+export { actions, middleware, reducer, selectors }
