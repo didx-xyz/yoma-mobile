9868c9d4117b711dcaea50251dfb9758bded86d3
Cameron Olivier <cameronolivier@gmail.com>
2021-10-19 08:56:00 +0200

    refactor(Qualifications): removing unused import

--- src/modules/Education/Form/EducationForm.tsx
+++ src/modules/Education/Form/EducationForm.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { ScrollView, View } from 'react-native'
 
-import { DropDownTags, FormLayout, InfoModal, Spinner } from '~/components'
+import { DropDownTags, FormLayout, InfoModal } from '~/components'
 import { ButtonSave } from '~/components/Button'
 import DateRangeSelect from '~/components/DateRangeSelect'
 import DropDown from '~/components/DropDown'
