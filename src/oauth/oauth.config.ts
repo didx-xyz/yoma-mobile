1f6d6a67e046cc932f007c5ed5cd86524778ad81
yoco_cam <cameron.olivier@yoco.com>
2022-04-09 22:15:00 +0200

    refactor(SSO): troubleshooting login and correct config

--- src/oauth/oauth.config.ts
+++ src/oauth/oauth.config.ts
@@ -7,6 +7,9 @@ const config: AuthConfiguration = {
   issuer: Env.OAUTH_BASE_PATH,
   scopes: ['openid', 'profile', 'yoma-api-v1', 'offline_access'],
   clientId: 'yoma-mobile-app',
-  redirectUrl: 'https://staging.app.yoma.africa/auth/sign-in',
+  // redirectUrl: 'https://staging.app.yoma.africa/auth/sign-in',
+  redirectUrl: 'com.yomamobile.auth://auth/sign-in',
 }
 export default config
+// com.yomamobile.auth://auth/sign-in <- redirect url on api
+//redirectUrl: 'https://staging.app.yoma.africa/auth/sign-in',
