f2e4a0d59d3a4e6427c8b1adc5127b2982adc5bd
yoco_cam <cameron.olivier@yoco.com>
2022-04-17 22:28:10 +0200

    tests: troubleshooting commits for user middleware

--- src/modules/User/User.middleware.test.ts
+++ src/modules/User/User.middleware.test.ts
@@ -1,6 +1,9 @@
+import { createMiddlewareMock } from '~/../tests/tests.utils'
 import { fetchUserFromOAuthSuccess } from '~/modules/Auth/Auth.reducer'
+import { OAuthUserResponse } from '~/modules/Auth/Auth.types'
+import { hydrateUser, setUser } from '~/modules/User/User.reducer'
+import { extractUserFromPayload } from '~/modules/User/User.utils'
 
-import { createMiddlewareMock } from '../../../tests/tests.utils'
 import * as SUT from './User.middleware'
 
 describe('modules/User/User.middleware', () => {
@@ -13,7 +16,7 @@ describe('modules/User/User.middleware', () => {
         status: 200,
         headers: {},
       }
-      const action = fetchUserFromOAuthSuccess(payloadMock as any)
+      const action = fetchUserFromOAuthSuccess(payloadMock as OAuthUserResponse)
 
       // when ... we respond to the loginSuccess action
       // @ts-ignore
@@ -24,6 +27,26 @@ describe('modules/User/User.middleware', () => {
       // then ...validate setUserOnAuthFlow
       expect(next).toHaveBeenCalledWith(action)
       expect(store.dispatch).toHaveBeenCalled()
+    })
+    it('should correctly set the user data', () => {
+      // given ... the fetchUserFromOAuthSuccess action is fired
+      const create = createMiddlewareMock(jest)
+      const payloadMock = {
+        data: {},
+        status: 200,
+        headers: {},
+      }
+      const action = fetchUserFromOAuthSuccess(payloadMock as OAuthUserResponse)
+      // @ts-ignore
+
+      // when ... we respond to the loginSuccess action
+      const { invoke, store } = create(SUT.setUserOnAuthFlow)
+      const userData = extractUserFromPayload(action)
+      invoke(action)
+
+      // then ... setUser should be called
+      expect(store.dispatch).toHaveBeenCalledWith(setUser(userData))
+      expect(store.dispatch).toHaveBeenCalledWith(hydrateUser())
     })
   })
 })
