c256b152f5ba57fa379ddd0b3d309c3dc8e50520
yoco_cam <cameron.olivier@yoco.com>
2022-04-17 22:26:44 +0200

    tests: troubleshooting commits for user middleware

--- src/modules/User/User.middleware.test.ts
+++ src/modules/User/User.middleware.test.ts
@@ -1,6 +1,29 @@
+import { fetchUserFromOAuthSuccess } from '~/modules/Auth/Auth.reducer'
+
+import { createMiddlewareMock } from '../../../tests/tests.utils'
+import * as SUT from './User.middleware'
+
 describe('modules/User/User.middleware', () => {
   describe('setUserOnAuthFlow', () => {
-    it('should correctly handle being called', () => {})
-    it('should correctly set the user data', () => {})
+    it('should correctly handle being called', () => {
+      // given ... the authLogin action is fired
+      const create = createMiddlewareMock(jest)
+      const payloadMock = {
+        data: {},
+        status: 200,
+        headers: {},
+      }
+      const action = fetchUserFromOAuthSuccess(payloadMock as any)
+
+      // when ... we respond to the loginSuccess action
+      // @ts-ignore
+      const { store, invoke, next } = create(SUT.setUserOnAuthFlow)
+
+      invoke(action)
+
+      // then ...validate setUserOnAuthFlow
+      expect(next).toHaveBeenCalledWith(action)
+      expect(store.dispatch).toHaveBeenCalled()
+    })
   })
 })
