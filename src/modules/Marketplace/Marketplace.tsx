d7b5890ede71f41677760220d7615d512ca35fe8
Cameron Olivier <cameronolivier@gmail.com>
2022-05-19 23:33:45 +0200

    feat(Marketplace): creating Coming soon page

--- src/modules/Marketplace/Marketplace.tsx
+++ src/modules/Marketplace/Marketplace.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-import Text, { HeaderLevels } from '~/components/Typography'
-
-import ViewContainer from '../../components/ViewContainer/ViewContainer'
-import styles from './Marketplace.styles'
+import { CircleSmallYellowGreenSplatter } from '~/assets/images'
+import ComingSoon from '~/components/ComingSoon'
+import { Colors } from '~/styles'
 
 const Marketplace = () => {
   const { t } = useTranslation()
   return (
-    <ViewContainer style={styles.container}>
-      <Text.Header level={HeaderLevels.H1}>{t('Marketplace')}</Text.Header>
-    </ViewContainer>
+    <ComingSoon
+      pageName={t('Marketplace')}
+      byLine={t('Record your growth on Yoma, unlock skills through opportunities and earn rewards!')}
+      heroBgColor={Colors.PrimaryGreen}
+      BottomImg={CircleSmallYellowGreenSplatter}
+    />
   )
 }
 
