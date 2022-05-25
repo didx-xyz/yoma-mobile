727760eaed704b4c8fdb95abe7820f5b6c979586
Cameron Olivier <cameronolivier@gmail.com>
2022-05-25 21:37:38 +0200

    feat(Utils: Strings): adding a trunc util

--- src/utils/strings.utils.ts
+++ src/utils/strings.utils.ts
@@ -1,7 +1,10 @@
-import { head, join, juxt, pipe, tail, toUpper, trim } from 'ramda'
+import { __, concat, gt, head, join, juxt, length, pipe, tail, take, toUpper, trim, unless } from 'ramda'
 
 export const getUppercasedHead = (data: string) => pipe(trim, head, toUpper)(data)
 
 export const capitalize = pipe(juxt([pipe(head, toUpper), tail]), join(''))
 
 export const textOrSpace = (condition: boolean, text: string) => (condition ? text : ' ')
+
+export const trunc = (text: string, len: number) =>
+  pipe(take(len), unless(pipe(length, gt(len)), pipe(trim, concat(__, '...'))), trim)(text)
