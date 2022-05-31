d1fcfbeb51a1972fc8fff9185bee2e595d32518f
Cameron Olivier <cameronolivier@gmail.com>
2022-05-31 23:10:39 +0200

    chore: adding a new env for staging, updating circle ci config to utelise for android build.

--- src/monitoring/index.ts
+++ src/monitoring/index.ts
@@ -7,9 +7,7 @@ export const routingInstrumentation = new Sentry.ReactNavigationInstrumentation(
 
 Sentry.init({
   dsn: 'https://9bec1ba1ff514183bc08dfbbaa609aad@o415689.ingest.sentry.io/6453219',
-  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-  // We recommend adjusting this value in production.
-  tracesSampleRate: 1.0,
+  tracesSampleRate: Env.SENTRY_TRACES_SAMPLE_RATE,
   environment: Env.ENVIRONMENT,
   integrations: [
     new Sentry.ReactNativeTracing({
