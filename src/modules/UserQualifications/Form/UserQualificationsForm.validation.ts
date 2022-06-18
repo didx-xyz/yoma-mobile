9d3448dfd14c7f3bb869f61ea3e895b66b2be97b
Cameron Olivier <cameronolivier@gmail.com>
2022-06-18 23:56:47 +0200

    chore: linting/code style fixes

--- src/modules/UserQualifications/Form/UserQualificationsForm.validation.ts
+++ src/modules/UserQualifications/Form/UserQualificationsForm.validation.ts
@@ -7,8 +7,8 @@ export const schema = yup.object().shape({
   startTime: yup.date().max(new Date(), 'Start date cannot be in the future').nullable().required('Required'),
   endTime: yup
     .date()
-    .when('startDate', (eventStartDate: any, schema: any) =>
-      eventStartDate ? schema.min(eventStartDate, 'End date cannot be before start date') : schema,
+    .when('startDate', (eventStartDate: any, s: any) =>
+      eventStartDate ? s.min(eventStartDate, 'End date cannot be before start date') : s,
     )
     .max(new Date(), 'End date cannot be in the future')
     .nullable()
