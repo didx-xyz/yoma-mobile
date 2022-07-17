b61b1cdee867312cb06d118d907cfc0ee40094fb
Cameron Olivier <cameronolivier@gmail.com>
2022-07-17 23:37:05 +0200

    refactor(CountrySelect): getting selector to return the correct shape of countries for populating the field component

--- src/modules/CountrySelectField/CountrySelect/CountrySelect.selector.ts
+++ src/modules/CountrySelectField/CountrySelect/CountrySelect.selector.ts
@@ -1,16 +1,19 @@
 import { createSelector } from '@reduxjs/toolkit'
 import { Country } from 'countries-list'
-import { mapObjIndexed, mergeRight, pipe, values } from 'ramda'
+import { evolve, mapObjIndexed, mergeRight, pipe, prop, values } from 'ramda'
 
 import { selectCountries } from '~/modules/Countries/Countries.selector'
 import { normaliseFn, sortIDs } from '~/redux/redux.utils'
 
-export default createSelector(selectCountries, countries => {
-  const { ids, entities } = pipe(
+export default createSelector(
+  selectCountries,
+  pipe(
+    prop('entities'),
     mapObjIndexed((value: Country, key: string) => mergeRight({ code: key }, value)),
     values,
     normaliseFn('name'),
-  )(countries)
-
-  return { ids: sortIDs(ids), entities }
-})
+    evolve({
+      ids: sortIDs,
+    }),
+  ),
+)
