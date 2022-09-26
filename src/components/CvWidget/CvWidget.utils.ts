aaea53c600eb7bdcc08db9dcf1bb561a1a1b4d4f
Cameron Olivier <cameronolivier@gmail.com>
2022-09-26 22:21:56 +0200

    chore: fixing tests, refactoring  shouldShowContent util

--- src/components/CvWidget/CvWidget.utils.ts
+++ src/components/CvWidget/CvWidget.utils.ts
@@ -1,8 +1,16 @@
-import { isNil } from 'ramda'
+import { isNil, lt } from 'ramda'
 import { ReactNode } from 'react'
+
+import { isNotNil } from '~/utils/ramda.utils'
 
 export const shouldShowContent = (
   count: number | undefined,
   shouldOverride: boolean | undefined,
   children: ReactNode,
-): boolean => (!!children && isNil(shouldOverride) ? (!!count && count > 0) || isNil(count) : shouldOverride || false)
+): boolean => {
+  if (isNotNil(children) && isNil(shouldOverride)) {
+    return (isNotNil(count) && lt(0, count)) || isNil(count)
+  }
+
+  return !!shouldOverride
+}
