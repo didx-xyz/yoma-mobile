ba2196ea8b5b1b51f7fd4ecfe8815f5be9c7421b
Cameron Olivier <cameronolivier@gmail.com>
2022-11-17 23:38:48 +0200

    chore: removing unusued types export from UserEducation

--- src/modules/UserEducation/index.ts
+++ src/modules/UserEducation/index.ts
@@ -1,7 +1,6 @@
 import * as middleware from './UserEducation.middleware'
 import reducer, * as actions from './UserEducation.reducer'
 import * as selectors from './UserEducation.selector'
-import * as types from './UserEducation.types'
 import * as utils from './UserEducation.utils'
 
-export { actions, reducer, middleware, types, selectors, utils }
+export { actions, reducer, middleware, selectors, utils }
